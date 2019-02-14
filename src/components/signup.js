58b54338dbd780404c2316997a1e1abbf26c4f6f
unknown <amkadesky@aol.com>
2019-02-13 20:47:51 -0600

    deleted comment

--- src/components/signup.js
+++ src/components/signup.js
@@ -1,5 +1,5 @@
 import React from 'react';
-//import '../index.css';
+
 import { Link, Redirect } from "react-router-dom";
 import { connect } from "react-redux";
 import { signupUser } from "../actions/index";

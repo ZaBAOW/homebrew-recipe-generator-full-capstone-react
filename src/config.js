532fce97410adc059104064c052bff9760dc8143
unknown <amkadesky@aol.com>
2019-04-25 21:27:15 -0500

    changed api origin url from local to heroku

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
-//exports.API_ORIGIN = process.env.REACT_APP_API_ORIGIN || "https://homebrew-recipe-generator-node.herokuapp.com"
+exports.API_ORIGIN = process.env.REACT_APP_API_ORIGIN || "https://homebrew-recipe-generator-node.herokuapp.com"
 
-exports.API_ORIGIN = "http://localhost:8080"
+//exports.API_ORIGIN = "http://localhost:8080"
